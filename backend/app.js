a4cc0c90cd5e063862752449c7e2ddb864571f08
iamsuigeneris <irewolelanre@gmail.com>
2021-08-22 14:46:43 +0100

    database added and password encrypted

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,15 @@
 const express = require("express")
-const app = express()
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const helmet = require("helmet")
 const morgan = require("morgan")
+const dotenv = require('dotenv')
 const userRouter = require("./routers/users")
 const ticketRouter = require("./routers/tickets")
 const handleError = require("./utils/errorHandler")
-const { nextTick } = require("process")
 
+
+const app = express()
 
 // API security
 app.use(helmet())
@@ -16,13 +17,33 @@ app.use(helmet())
 //handle CORS error
 app.use(cors())
 
-//Logger
-app.use(morgan("tiny"))
+dotenv.config()
 
 // Set bodyParser
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+// MongoDB connnection setup
+const mongoose = require("mongoose")
+
+mongoose.connect(, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology:true
+})
+
+if (process.env.NODE_ENV !== "production") {
+    const mDb = mongoose.connection
+    mDb.on("open", () => {
+        console.log("MongoDB is connected")
+    })
+    mDb.on("error", (error) => {
+        console.log(error)
+    })
+    //Logger
+    app.use(morgan("tiny"))
+}
 
 // use Routers
 app.use("/v1/user", userRouter)
@@ -39,6 +60,7 @@ app.use((error, req, res, next) => {
 })
 
 const port = process.env.PORT || 3001
+
 app.listen(port, () => {
     console.log(`Backend is running on ${port}`)
 })
--- backend/routers/users.js
+++ backend/routers/users.js
@@ -1,8 +1,30 @@
 const express = require("express")
 const router = express.Router()
 
+const { insertUser } = require("../model/user/UserModel")
+const { hashPassword } = require("../helpers/bcrypt")
+
 router.all("/", (req, res, next) => {
-    res.json({ message: "return from user router" })
+    // res.json({ message: "return from user router" })
+    next()
+})
+
+router.post("/", async (req, res) => {
+    const {name,company,address,phone,email,password} = req.body
+    try {
+        //hash password
+        const hashedPass = await hashPassword(password)
+        const newUserObj = {
+            name, company, address, phone, email, password:hashedPass 
+        }
+        const result = await insertUser(newUserObj)
+        console.log(result)
+        res.json({ message: "New User Created", result })
+    } catch (error) {
+        console.log(error)
+        res.json({status:'error', message:error.message})
+    }
+
 })
 
 module.exports = router
